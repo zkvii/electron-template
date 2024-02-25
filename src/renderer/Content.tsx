f1be0fec343ef60fb6c087d540f991a1c13cf9df
kri ly <2330362931@qq.com>
2024-02-25 22:30:51 +0800

    add new readme

--- src/renderer/Content.tsx
+++ src/renderer/Content.tsx
@@ -1,14 +1,11 @@
 import './Content.scss';
 
-const ToggleTheme = async () => {
-  await window.darkMode.toggle();
-};
+
 
 export function Content() {
   return (
     <div className="content">
-      <h1>Electron React Boilerplate</h1>
-      <button onClick={ToggleTheme}>Toggle Theme</button>
+
     </div>
   );
 }
