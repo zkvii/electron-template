cfd6b1b99adb6bbbaf23c4bc91e9fb3f88f8a50e
kri ly <2330362931@qq.com>
2024-02-25 23:13:29 +0800

    fix all

--- src/renderer/Content.tsx
+++ src/renderer/Content.tsx
@@ -1,11 +1,41 @@
 import './Content.scss';
+import HomeOutlined from '@ant-design/icons/HomeOutlined';
+import search from '../../assets/svg/search.svg';
 
 
+function Libs() {
+  const libs = ['lib1'];
+  return (
+    <div className="main-libs">
+      {libs.map((lib) => (
+        <div className="menu-item">
+          <HomeOutlined />
+          <span className="menu-item-text">{lib}</span>
+
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export function Content() {
+
+
   return (
     <div className="content">
+      <div className="left-panel">
+        <div className="left-panel__header">
+          <input className="left-panel__header__search-input" />
+          <img className="left-panel__header__search-icon" src={search} alt="search" />
+        </div>
+        <div className="left-panel__content">
+          <div>
+            <Libs />
+          </div>
 
+        </div>
+      </div>
+      <div className="right-panel"></div>
     </div>
   );
 }
